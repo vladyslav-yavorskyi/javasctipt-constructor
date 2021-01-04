ddd3ec00052fc2bec7ea7e5b0b891a9ee63104a1
Vladyslav <vlad09vlad09@gmail.com>
2021-01-04 13:33:31 +0100

    change model structure

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const site = document.querySelector('#site');
 
 model.forEach((block) => {
   const generate = tempaltes[block.type];
-
+  console.log(generate);
   if (generate) {
     const html = generate(block);
     site.insertAdjacentHTML('beforeend', html);
--- src/model.js
+++ src/model.js
@@ -1,47 +1,32 @@
 import img from './img/app.jpg';
+import { Block } from './classes/blocks';
 
 export const model = [
-  {
-    type: 'title',
-    value: 'Web constructor on pure JavaScript!',
-    options: {
-      tag: 'h1',
-      styles:
-        'background: linear-gradient(90deg, rgba(255,233,133,1) 0%, rgba(255,148,16,1) 97%);; color: #fff; text-align: center; font-weight: 600; padding: 0.5rem;',
-    },
-  },
-  {
-    type: 'text',
-    value: 'The web application on pure JS, without any frameworks',
-    options: {
-      styles:
-        'background: linear-gradient(212deg, rgba(133,207,255,1) 0%, rgba(16,61,255,1) 97%);; color: yellow; text-align: center;',
-    },
-  },
-  {
-    type: 'title',
-    value: 'About Author:',
-    options: {
-      tag: 'h2',
-      styles: 'font-weight: 500;',
-    },
-  },
-  {
-    type: 'textColumns',
-    value: [
+  new Block('title', 'Web constructor on pure JavaScript!', {
+    tag: 'h1',
+    styles:
+      'background: linear-gradient(90deg, rgba(255,233,133,1) 0%, rgba(255,148,16,1) 97%);; color: #fff; text-align: center; font-weight: 600; padding: 0.5rem;',
+  }),
+  new Block('text', 'The web application on pure JS, without any frameworks', {
+    styles:
+      'background: linear-gradient(212deg, rgba(133,207,255,1) 0%, rgba(16,61,255,1) 97%);; color: yellow; text-align: center;',
+  }),
+  new Block('title', 'About Author:', {
+    tag: 'h2',
+    styles: 'font-weight: 500;',
+  }),
+  new Block(
+    'textColumns',
+    [
       'name: Vladyslav Yavorksyi😃',
       'job: a profesional Front-end Developer😎',
       'contact: vlad09vlad09@gmail.com📧',
     ],
-    options: {
+    {
       styles: 'text-align: center; padding: 1rem; border: 1px solid #ccc;',
-    },
-  },
-  {
-    type: 'image',
-    value: img,
-    options: {
-      styles: ' display: flex; justify-content: center;',
-    },
-  },
+    }
+  ),
+  new Block('image', img, {
+    styles: ' display: flex; justify-content: center;',
+  }),
 ];
