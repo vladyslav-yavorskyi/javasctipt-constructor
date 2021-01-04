6813f08af9c18b9c26b26809c42c1ae4900773fe
Vladyslav <vlad09vlad09@gmail.com>
2021-01-04 13:04:44 +0100

    added gradient style

--- src/model.js
+++ src/model.js
@@ -7,14 +7,15 @@ export const model = [
     options: {
       tag: 'h1',
       styles:
-        'background: darkred; color: #fff; text-align: center; font-weight: 600; padding: 0.5rem;',
+        'background: linear-gradient(90deg, rgba(255,233,133,1) 0%, rgba(255,148,16,1) 97%);; color: #fff; text-align: center; font-weight: 600; padding: 0.5rem;',
     },
   },
   {
     type: 'text',
     value: 'The web application on pure JS, without any frameworks',
     options: {
-      styles: 'background: darkblue; color: yellow; text-align: center;',
+      styles:
+        'background: linear-gradient(212deg, rgba(133,207,255,1) 0%, rgba(16,61,255,1) 97%);; color: yellow; text-align: center;',
     },
   },
   {
